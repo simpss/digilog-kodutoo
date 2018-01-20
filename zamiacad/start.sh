305295db9635ad7e24c860d356d4d8ad6b02f7b9
Siim Talvik <siim@talvik.eu>
2018-01-20 07:19:29 +0200

    eclipse persistance configured.

--- zamiacad/start.sh
+++ zamiacad/start.sh
@@ -1,5 +1,6 @@
 docker run -ti --rm \
            -e DISPLAY=$DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v "`pwd`":/workspace \
+           -v `pwd`/eclipse:/home/developer \
+           -v `pwd`/eclipse:/workspace \
 simpss/zamiacad:v4.4.1
