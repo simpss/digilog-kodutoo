912dffdc323aefc86fc26a7df9b7ba3883b7e3e9
Siim Talvik <siim@talvik.eu>
2018-01-20 08:32:18 +0200

    task 7 simulator x1...x4 done.

--- zamiacad/eclipse/homework/test.vhdl
+++ zamiacad/eclipse/homework/test.vhdl
@@ -7,10 +7,13 @@ end test;
 architecture bench of test is
 
   signal n4x1, n4x2, n4x3, n4x4: bit;
+  signal n7x1, n7x2, n7x3, n7x4: bit;
+  signal n8x1, n8x2, n8x3, n8x4: bit;
+  
 
   signal x1, x2, x3, x4: bit;
 
-  signal y4, y9, y10: bit;
+  signal y4, y7, y8, y9, y10: bit;
 
   signal x1n, x2n, x3n, y10n: bit;
 
@@ -64,7 +67,51 @@ begin
 
         (x1 and not x2 and x3 and not x4);
 
+  -- Punkti 7 tulemus
 
+  n7x1 <= '0', '1' after 80 ns, '0' after 160 ns;
+
+  n7x2 <= '0', '1' after 40 ns, '0' after 80 ns, '1' after 120 ns, '0' after 160 ns;
+  
+  n7x3 <= '0', '1' after 20 ns, '0' after 40 ns, '1' after 60 ns, '0' after 80 ns, '1' after 100 ns, '0' after 120 ns, '1' after 140 ns;
+  
+  n7x4 <= '0', '1' after 10 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns, '1' after 50 ns, '0' after 60 ns, '1' after 70 ns, 
+  '0' after 80 ns, '1' after 90 ns, '0' after 100 ns, '1' after 110 ns, '0' after 120 ns, '1' after 130 ns, '0' after 140 ns, 
+  '1' after 150 ns, '0' after 160 ns;
+
+  y7 <= (not x1 and not x2 and not x3 and x4) or
+
+        (not x1 and not x2 and x3 and not x4) or
+
+        (not x1 and not x2 and x3 and x4) or
+
+        (not x1 and x2 and x3 and not x4) or
+
+        (x1 and not x2 and not x3 and x4) or
+
+        (x1 and not x2 and x3 and not x4);
+        
+  -- Punkti 8 tulemus
+
+  n8x1 <= '0', '0' after 20 ns, '0' after 40 ns, '0' after 60 ns, '1' after 80 ns, '1' after 100 ns;
+
+  n8x2 <= '0', '0' after 20 ns, '0' after 40 ns, '1' after 60 ns, '0' after 80 ns, '0' after 100 ns;
+
+  n8x3 <= '0', '1' after 20 ns, '1' after 40 ns, '1' after 60 ns, '0' after 80 ns, '1' after 100 ns;
+
+  n8x4 <= '1', '0' after 20 ns, '1' after 40 ns, '0' after 60 ns, '1' after 80 ns, '0' after 100 ns;
+
+  y8 <= (not x1 and not x2 and not x3 and x4) or
+
+        (not x1 and not x2 and x3 and not x4) or
+
+        (not x1 and not x2 and x3 and x4) or
+
+        (not x1 and x2 and x3 and not x4) or
+
+        (x1 and not x2 and not x3 and x4) or
+
+        (x1 and not x2 and x3 and not x4);
 
   -- Punkti 9 tulemus
 
